2a58982b07ddaa09212a211796670ff669d99d30
hexxt-git <zeghdns@gmail.com>
2025-10-05 10:58:19 +0100

    *

--- web/.wrangler/tmp/deploy-WgTXaz/index.js
+++
@@ -1,8 +0,0 @@
-import { B } from "./assets/worker-entry-BO25p6wY.js";
-import "node:events";
-import "node:stream";
-import "node:async_hooks";
-import "node:stream/web";
-export {
-  B as default
-};
